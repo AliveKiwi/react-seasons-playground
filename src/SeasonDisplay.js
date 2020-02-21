dd09aeb9fe83f3b0d1f034b2412a58f20ce3439c
Prateek Jaiswal <prateekjais.dev@gmail.com>
2020-02-22 01:28:27 +0530

    Updated with Component Life Cycle Methods

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -9,12 +9,39 @@ class SeasonDisplay extends React.Component {
       errorMessage: ''
     };
 
+    // console.log('Constructor called 1st');
+  }
+
+  componentWillMount() {
+    // console.log('Component Will Mount called 2nd ');
+  }
+
+  componentDidMount() {
+    // console.log('Component Did Mounted called 4th');
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
     );
   }
+
+  componentWillUpdate() {
+    // console.log('Component Will Update called 5th ');
+  }
+
+  componentDidUpdate() {
+    // console.log('Component Did Update called 7th ');
+  }
+
+  componentDidCatch() {
+    // console.log('Caught Error');
+  }
+
+  componentWillUnmount() {
+    // console.log('Component Will Unmount called last ');
+  }
+
   render() {
+    // console.log('Inisde Render called 3rd');
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error:{this.state.errorMessage}</div>;
     }
