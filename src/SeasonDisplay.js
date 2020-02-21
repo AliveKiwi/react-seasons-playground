be2b16bc61eec7edc36457ebcf86aacbbab9bc07
Prateek Jaiswal <prateekjais.dev@gmail.com>
2020-02-22 02:51:11 +0530

    Added css, and some style

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,55 +1,31 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-class SeasonDisplay extends React.Component {
-  constructor(props) {
-    super(props);
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    iconName: 'sun'
+  },
+  winter: { text: "Burr, it's chilly", iconName: 'snowflake' }
+};
 
-    this.state = {
-      lat: null, //set to null if value is number
-      errorMessage: ''
-    };
-
-    // console.log('Constructor called 1st');
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
   }
-
-  componentWillMount() {
-    // console.log('Component Will Mount called 2nd ');
-  }
-
-  componentDidMount() {
-    // console.log('Component Did Mounted called 4th');
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
-  }
-
-  componentWillUpdate() {
-    // console.log('Component Will Update called 5th ');
-  }
-
-  componentDidUpdate() {
-    // console.log('Component Did Update called 7th ');
-  }
-
-  componentDidCatch() {
-    // console.log('Caught Error');
-  }
-
-  componentWillUnmount() {
-    // console.log('Component Will Unmount called last ');
-  }
-
-  render() {
-    // console.log('Inisde Render called 3rd');
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error:{this.state.errorMessage}</div>;
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude:{this.state.lat}</div>;
-    }
-    return <div>Loading!</div>;
-  }
-}
+};
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
+};
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      errorMessage: ''
+    };
+    // console.log('Constructor called 1st');
+  }
+
+  // componentWillMount() {} console.log('Component Will Mount called 2nd ');
+
+  componentDidMount() {
+    // console.log('Component Did Mounted called 4th');
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  // componentWillUpdate() {} console.log('Component Will Update called 5th ');
+  // componentDidUpdate() {} console.log('Component Did Update called 7th ');
+  // componentDidCatch() {} console.log('Caught Error');
+  // componentWillUnmount() {} console.log('Component Will Unmount called last ');
+
   render() {
-    return <SeasonDisplay></SeasonDisplay>;
+    // console.log('Inisde Render called 3rd');
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error:{this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat}></SeasonDisplay>;
+    }
+    return <Spinner />;
   }
 }
 
